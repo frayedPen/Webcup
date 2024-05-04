49db6516ccfb7a64f1b3625d7c302a9424a314a5
Admin <buldan.556@gmail.com>
2024-05-04 17:44:50 +0400

    fix error on pages name

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,13 +7,24 @@
     import { Navbar, NavBrand,Input, NavLi, NavUl, Button, NavHamburger,Thumbnails,ButtonGroup,Card, Avatar, } from 'flowbite-svelte';
 
     import landingImage from '$lib/img/landingImage.jpg';
+    import beerBurger from '$lib/img/beer and burger.jpg';
 </script>
 
+<!-- Landing -->
 <div class="bravo-landing-image bg-cover bg-no-repeat relative" style="background-image: url('{landingImage}');">
 
 </div>
 
 <BravoMealBuilder />
+
+<div class="h-screen bg-white grid grid-cols-2">
+    <div>
+        <h1 class="text-9xl">Our Speciality</h1>
+        <h2 class="text-8xl">Beers & Burger</h2>
+    </div>
+    
+    <img class="col-start-2" src="{beerBurger}" alt="Beer and burger">
+</div>
 
 <style>
     :global(.bravo-landing-image) {
